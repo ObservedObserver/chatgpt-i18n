9efe1215ed427b35da893ad6d495a98e0cdfd2e9
ObservedObserver <270001151@qq.com>
2023-03-10 01:21:37 +0800

    feat: add vercel analytic

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import appRouter from './App'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
+import { inject } from '@vercel/analytics';
+
+inject();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
